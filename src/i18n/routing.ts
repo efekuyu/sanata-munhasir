8dc1a01799c22f71e66a0a76615b9f889fc32821
Efe Kuyu <efekuyu@Apple-M1-Pro.local>
2026-05-04 00:55:37 +0100

    Disable browser locale detection

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ import { createNavigation } from 'next-intl/navigation';
 export const routing = defineRouting({
   locales: ['tr', 'en'],
   defaultLocale: 'tr',
+  localeDetection: false,
   pathnames: {
     '/': '/',
     '/about': {
